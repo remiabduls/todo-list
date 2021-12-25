cd6cf3e82aef944dfa6bbdaa26d34e4b2b192387
Remi <remiabduls@gmail.com>
2021-12-25 22:10:32 +0100

    commit to 'master'

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,8 +9,11 @@ function Todo({ text,todo, todos, setTodos }) {
             if(item.id === todo.id){
                 return {
                     ...item, completed: !item.completed
-                }
+                };
+                
             }
+            return item;
+            
         }))
     }
   
